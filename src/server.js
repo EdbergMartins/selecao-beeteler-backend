09de6caeb5e74b8adf9866afacd455e4d77f3359
EdbergMartins <edbergmartins@gmail.com>
2023-12-20 13:39:06 -0300

    insert .env and remove create db

--- src/server.js
+++ src/server.js
@@ -2,18 +2,16 @@ const express = require('express');
 const auth = require('./routes/auth');
 const app = express();
 const cors = require('cors');
-const createDB = require('./models/create');
 const populateDB = require('./models/populate');
 
 app.use(express.json());
 app.use(cors());
 app.use(auth)
 
-app.listen(8080, () => {
-  console.log('Server is listening on port 8080');
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
 
 (async () => {
-  await createDB();
   await populateDB();
 })();
